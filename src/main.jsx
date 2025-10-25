eb4d38a9030a5554a8c1e15ee561c48bb903c0d7
Shagon1992 <129582871+Shagon1992@users.noreply.github.com>
2025-10-25 11:59:39 +0700

    Import Leaflet CSS for map styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx"; // atau "./App.js" sesuai nama file kamu
 import "./index.css"; // kalau ada CSS global
+import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
